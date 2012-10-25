24d59e9dbd4e38778711b80519cc72ac26a87a67
Gnuk <gnuk@mailoo.org>
2012-10-25 17:52:12 +0200

    Ajout des REGEX pour le réducteur (récupération d'un fichier 3SAT fonctionne)

--- src/Reducteur.java
+++ src/Reducteur.java
@@ -1,19 +1,67 @@
 import java.io.File;
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 public class Reducteur {
 	private File input3SAT;
 	private ThreeSAT threeSat;
-
+	private TraitementFichier fichier;
+	
+	/**
+	 * Constructeur du réducteur
+	 * @param filename Le num du fichier
+	 * @throws IOException
+	 */
 	public Reducteur(String filename) throws IOException{
 		this.input3SAT = new File(filename);
-		this.threeSat = this.getThreeSat();
+		this.getThreeSat();
 	}
 
-	private ThreeSAT getThreeSat() throws IOException {
-		TraitementFichier fichier = new TraitementFichier(this.input3SAT);
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Récupère les informations du fichier 3SAT
+	 * @throws IOException
+	 */
+	private void getThreeSat() throws IOException {
+		this.fichier = new TraitementFichier(this.input3SAT);
+		String infos = this.fichier.getString();
+		System.out.println(infos);
+		String strArray[] = infos.split("\\r?\\n");
+		ArrayList<String[]> numbers = new ArrayList<String[]>();
+		String line;
+		String number = "";
+		for (int i = 0; i < strArray.length; i++) {
+			if (strArray[i].matches("^[0-9]*$")) {
+				number = strArray[i];
+			}
+			if (strArray[i].matches("^\\([-0-9|0-9|,| ]*\\)")) {
+				line = strArray[i].replaceAll("\\(|\\)", "");
+				String lineNumbers[] = line.split(",");
+				numbers.add(lineNumbers);
+			}
+		}
+		
+		// Affichage
+		if(number != ""){
+			System.out.println("number: " + number);
+		}
+		this.threeSat = new ThreeSAT(Integer.valueOf(number).intValue());
+		for (int i = 0; i < numbers.size(); i++) {
+			int[] temp = new int[this.threeSat.getN()];
+			for(int y = 0; y<this.threeSat.getN(); y++){
+				temp[y] = Integer.valueOf((numbers.get(i)[y]).trim()).intValue();
+				// Affichage
+				System.out.println("values.get(" + i + ")["+y+"] => "+ temp[y]);
+			}
+			this.threeSat.addExpression(temp);
+		}
+	}
+	
+	/**
+	 * Lancement du réducteur
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		Reducteur reducteur = new Reducteur("input3SAT");
 	}
 }
--- src/ThreeSAT.java
+++ src/ThreeSAT.java
@@ -6,12 +6,22 @@ public class ThreeSAT {
 	private ArrayList<String>errors;
 	private ArrayList<int[]>expr;
 	private boolean valid;
+	
+	/**
+	 * Constructeur de 3SAT
+	 * @param n
+	 */
 	public ThreeSAT(int n){
 		this.n=n;
 		this.valid = true;
 		this.expr = new ArrayList<int[]>();
 		this.errors = new ArrayList<String>();
 	}
+	
+	/**
+	 * Une ligne de 3SAT
+	 * @param expr
+	 */
 	public void addExpression(int[] expr){
 		if(expr.length == this.n){
 			this.expr.add(expr);
@@ -21,15 +31,37 @@ public class ThreeSAT {
 			this.valid = false;
 		}
 	}
+	
+	/**
+	 * Indique si le 3SAT est valide
+	 * @return
+	 */
 	public boolean isValid() {
 		return valid;
 	}
+	
+	/**
+	 * Récupère les expressions du 3SAT
+	 * @return
+	 */
 	public ArrayList<int[]> getExpr() {
 		return expr;
 	}
+	
+	/**
+	 * Affiche les erreurs s'il y en a
+	 */
 	private void showErrors() {
 		for(int i = 0; i < this.errors.size(); i++){
 			System.out.println("Erreur" + this.errors.get(i));
 		}
+	}
+	
+	/**
+	 * Récupère le nombre associé au 3SAT
+	 * @return
+	 */
+	public int getN() {
+		return n;
 	}
 }
